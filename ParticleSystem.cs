a6743b59f03066dd62a138633a2fb128a384a506
Zoey Hamm <339622581@GoTVDSB.ca>
2025-04-15 13:53:46 -0400

    Added System class that actual moves the particles

---
+++ ParticleSystem.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_5._5___Particle_Effects
+{
+    public class ParticleSystem
+    {
+        private Random _generator;
+        public Vector2 _emitterLocation;
+        private List<Particle> _particles;
+        private List<Texture2D> _textures;
+
+        public ParticleSystem(List<Texture2D> textures, Vector2 location)
+        {
+            _emitterLocation = location;
+            this._textures = textures;
+            this._particles = new List<Particle>();
+            _generator = new Random();
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = _textures[_generator.Next(_textures.Count)];
+            Vector2 position = _emitterLocation;
+            Vector2 velocity = new Vector2(
+                    1f * (float)(_generator.NextDouble() * 2 - 1),
+                    1f * (float)(_generator.NextDouble() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(_generator.NextDouble() * 2 - 1);
+            Color color = new Color(
+                    (float)_generator.NextDouble(),
+                    (float)_generator.NextDouble(),
+                    (float)_generator.NextDouble());
+            float size = (float)_generator.NextDouble();
+            int time = 20 + _generator.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, time);
+        }
+
+        public void Update()
+        {
+            int total = 10;
+
+            for (int i = 0; i < total; i++)
+            {
+                _particles.Add(GenerateNewParticle());
+            }
+
+            for (int particle = 0; particle < _particles.Count; particle++)
+            {
+                _particles[particle].Update();
+                if (_particles[particle]._time <= 0)
+                {
+                    _particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int index = 0; index < _particles.Count; index++)
+            {
+                _particles[index].Draw(spriteBatch);
+            }
+            spriteBatch.End();
+        }
+
+    }
+}
